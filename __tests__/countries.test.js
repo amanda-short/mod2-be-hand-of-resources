dcee9470f985f2b911c9b5e0728ee40d643b6dbc
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-10 12:35:39 -0800

    route 2, READ GET all built and tested, passed

---
+++ __tests__/countries.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('countries routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /countries should return a list of countries', async () => {
+    const resp = await request(app).get('/countries');
+    expect(resp.body.length).toEqual(5);
+    const italy = resp.body.find((country) => country.id === '1');
+    expect(italy).toHaveProperty('name', 'Italy');
+    expect(italy).toHaveProperty('language', 'Italian');
+    expect(italy).toHaveProperty('population', '59 million');
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
