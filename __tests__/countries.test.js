73dc0a0a3365e249f83776f985afcb902137dddb
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-10 13:04:54 -0800

    route 2 countries, DELETE function added and route complete, passed testing

--- __tests__/countries.test.js
+++ __tests__/countries.test.js
@@ -47,6 +47,13 @@ describe('countries routes', () => {
     expect(resp.body.name).toEqual('Belgium');
   });
 
+  it('DELETE /countries/:id should delete a country', async () => {
+    const resp = await request(app).delete('/countries/2');
+    expect(resp.status).toEqual(200);
+    const { body } = await request(app).get('/countries/2');
+    expect(body).toEqual(null);
+  });
+
   afterAll(() => {
     pool.end();
   });
